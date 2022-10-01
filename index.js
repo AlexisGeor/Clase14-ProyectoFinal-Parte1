41d82bc2ca0b02a10a2eb9c518fa19acebe372ff
Alexis Georgopulos [Windows] <ageorgopulos@frba.utn.edu.ar>
2022-09-30 21:52:14 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const app = express();
+const httpServer = new HttpServer(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+
+app.get("/", (req, res) => {
+    res.json({
+        ok: true
+    });
+});
+
+httpServer.listen(PORT, () => console.log("Servidor corriendo en el puerto", PORT));
